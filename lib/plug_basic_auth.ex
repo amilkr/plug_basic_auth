92112685b64b0886855bbc177bb40e8ae6e577c7
amilkr <amilcar.almonacid@gmail.com>
2015-10-30 23:16:06 -0700

    Implement validation callback. Remove username and password key

--- lib/plug_basic_auth.ex
+++ lib/plug_basic_auth.ex
@@ -2,25 +2,26 @@ defmodule PlugBasicAuth do
   @moduledoc """
   A plug for protecting routers with HTTP Basic Auth.
 
-  It expects a `:username` and `:password` to be passed as
-  binaries at initialization.
+  It expects a `:validation` to be passed as &Mod.fun/1 at initialization.
 
   The user will be prompted for a username and password upon
   accessing any of the routes using this plug.
 
-  If the username and password are correct, the user will be
-  able to access the page.
+  The `:validation` callback would be used to decide if the username and
+  password are correct:
 
-  If the username and password are incorrect, the user will be
-  prompted to enter them again.
+  If it returns `{conn, :authorized}`, the username and password are correct
+  and the user will be able to access the page.
+
+  If it returns `{conn, :unauthorized}`, the username and password are incorrect
+  and the user will be prompted to enter them again.
 
   ## Example
 
       defmodule TopSecret do
         import Plug.Conn
         use Plug.Router
-
-        plug PlugBasicAuth, username: "Snorky", password: "Capone"
+        plug PlugBasicAuth, validation: &TopSecret.is_authorized/1
         plug :match
         plug :dispatch
 
@@ -29,6 +30,9 @@ defmodule PlugBasicAuth do
           |> put_resp_content_type("text/plain")
           |> send_resp(200, "Welcome to the party.")
         end
+
+        def is_authorized({conn, {"Snorky", "Capone"}}), do: {conn, :authorized}
+        def is_authorized({conn, _}), do: {conn, :unauthorized}
       end
   """
 
@@ -38,16 +42,15 @@ defmodule PlugBasicAuth do
                            halt:            1]
 
   def init(opts) do
-    username = Keyword.fetch!(opts, :username)
-    password = Keyword.fetch!(opts, :password)
-    username <> ":" <> password
+    Keyword.fetch!(opts, :validation)
   end
 
-  def call(conn, server_creds) do
+  def call(conn, validation) do
     conn
     |> get_auth_header
     |> parse_auth
-    |> check_creds(server_creds)
+    |> validation.()
+    |> respond
   end
 
   defp get_auth_header(conn) do
@@ -57,16 +60,13 @@ defmodule PlugBasicAuth do
 
   defp parse_auth({conn, ["Basic " <> encoded_creds | _]}) do
     {:ok, decoded_creds} = Base.decode64(encoded_creds)
-    {conn, decoded_creds}
+    [usr, pwd] = String.split(decoded_creds, ":", parts: 2)
+    {conn, {usr, pwd}}
   end
   defp parse_auth({conn, _}), do: {conn, nil}
 
-  defp check_creds({conn, decoded_creds}, server_creds) when decoded_creds == server_creds do
-    conn
-  end
-  defp check_creds({conn, _}, _), do: respond_with_login(conn)
-
-  defp respond_with_login(conn) do
+  defp respond({conn, :authorized}), do: conn
+  defp respond({conn, :unauthorized}) do
     conn
     |> put_resp_header("www-authenticate", "Basic realm=\"Private Area\"")
     |> send_resp(401, "")
